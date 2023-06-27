47c95304e5095d1b7bef962af9fe031d4b469345
adwait77 <137475504+adwait77@users.noreply.github.com>
2023-06-27 20:12:49 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,76 @@
+print("arithmetic operators: ")
+a=20
+b=30
+print("addition: ",a+b)
+print("subtraction: ",a-b)
+print("multiplication: ",a*b)
+print("division: ",a/b)
+print("floor division: ",a//b)
+print("exponent: ",a**b)
+print("mod: ",a%b)
+print("")
+print("assignment operators: ")
+c=50
+c+=1
+print("+=",c)
+c=50
+c-=1
+print("-=",c)
+c=50
+c*=1
+print("*=",c)
+c=50
+c/=1
+print("/=",c)
+c=50
+c**=1
+print("**=",c)
+c=50
+c//=1
+print("//=",c)
+c=50
+c%=1
+print("%=",c)
+
+print("")
+print("comparision operators / Relational operators: ")
+d=10
+e=5
+print(">=",d>=e)
+print("<=",d<=e)
+print(">",d>e)
+print("<",d<e)
+print("==",d==e)
+print("!=",d!=e)
+
+print("")
+print("logical operators: ")
+a=True
+b=False
+print("and",a and b)
+print("or",a or b)
+print("not",not b)
+
+print("")
+print("bitwise operators: ")
+a=10
+b=5
+print("bitwise &",a&b)
+print("bitwise |",a|b)
+print("bitwise ^",a^b)
+print("bitwise >>",a>>b)
+print("bitwise <<",a<<b)
+
+print("")
+print("identity operators: ")
+a=30
+b=40
+print("is ",a is b)
+print("is not ",a is not b)
+
+print("")
+print("membership operators: ")
+a="adwait"
+print('a' in a)
+print("m" not in a)
+
