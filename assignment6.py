e936d4e4a5099f8c401085f71804098c98be5469
adwait77 <137475504+adwait77@users.noreply.github.com>
2023-07-02 00:07:32 +0530

    Update assignment6.py

--- assignment6.py
+++ assignment6.py
@@ -1,42 +1,42 @@
 def ds(roll_no, name):
-    my_list = [roll_no, name]
-    my_tuple = (roll_no, name)
-    my_set = {roll_no, name}
-    my_dict = {'Roll No': roll_no, 'Name': name}
+    list = [roll_no, name]
+    tuple = (roll_no, name)
+    set = {roll_no, name}
+    dict = {'Roll No': roll_no, 'Name': name}
     
     print("Initial values:")
-    print("List:", my_list)
-    print("Tuple:", my_tuple)
-    print("Set:", my_set)
-    print("Dictionary:", my_dict)
+    print("List:", list)
+    print("Tuple:", tuple)
+    print("Set:", set)
+    print("Dictionary:", dict)
     
     new_roll_no = input("Enter new roll number: ")
     new_name = input("Enter new name: ")
     
-    my_list[0] = new_roll_no
-    my_list[1] = new_name
+    list[0] = new_roll_no
+    list[1] = new_name
     
-    my_tuple = (new_roll_no, new_name)
+    tuple = (new_roll_no, new_name)
     
-    my_set.remove(roll_no)
-    my_set.add(new_roll_no)
-    my_set.remove(name)
-    my_set.add(new_name)
+    set.remove(roll_no)
+    set.add(new_roll_no)
+    set.remove(name)
+    set.add(new_name)
     
-    my_dict['Roll No'] = new_roll_no
-    my_dict['Name'] = new_name
+    dict['Roll No'] = new_roll_no
+    dict['Name'] = new_name
     
     print("Updated values:")
-    print("List:", my_list)
-    print("Tuple:", my_tuple)
-    print("Set:", my_set)
-    print("Dictionary:", my_dict)
-    
-    del my_list
-    del my_tuple
-    del my_set
-    del my_dict
+    print("List:", list)
+    print("Tuple:", tuple)
+    print("Set:", set)
+    print("Dictionary:", dict)
+
+    del list
+    del tuple
+    del set
+    del dict
     
     print("Data structures deleted.")
 
-ds('123', 'John')
+ds('123', 'Adwait')
