74718977c8fe735549cd3eb714874f755f9dd4b5
adwait77 <137475504+adwait77@users.noreply.github.com>
2023-07-04 23:48:27 +0530

    Update assignment10.py

--- assignment10.py
+++ assignment10.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 import webbrowser as wb
+
 obj=tk.Tk()
 e=tk.Entry(obj,font=("Comic Sans",50),width=50)
 e.grid(row=0,column=0)
 
 def display():
     s=e.get()
-    print(s)  
-    wb.open(f"www.youtube.com/results?search_query={s}") 
+    wb.open(f"www.youtube.com/results?search_query={s}")
+    print("'",s,"' Navigating to youtube") 
 
 b1=tk.Button(obj,text="Search",font=("Comic Sans",50),bg="cyan",height=1,width=6,command=display)
 b1.grid(row=1,column=0)
