9d37fe65391cda1eaf147200da4c4b626bde05b4
adwait77 <137475504+adwait77@users.noreply.github.com>
2023-07-04 23:41:30 +0530

    Add files via upload

---
+++ assignment10.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import webbrowser as wb
+obj=tk.Tk()
+e=tk.Entry(obj,font=("Comic Sans",50),width=50)
+e.grid(row=0,column=0)
+
+def display():
+    s=e.get()
+    print(s)  
+    wb.open(f"www.youtube.com/results?search_query={s}") 
+
+b1=tk.Button(obj,text="Search",font=("Comic Sans",50),bg="cyan",height=1,width=6,command=display)
+b1.grid(row=1,column=0)
+b=tk.Button(obj,text="Close",font=("Comic Sans",50),bg="pink",height=1,width=6,command=obj.destroy)
+b.grid(row=2,column=0)
+obj.mainloop()
