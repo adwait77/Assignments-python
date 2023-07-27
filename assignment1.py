1a7cf68251e83cc8c2e26ccab24e7f790fcf8ac7
adwait77 <137475504+adwait77@users.noreply.github.com>
2023-07-27 16:28:43 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,7 @@
-n1=1
-n2=2
-n3=3
-average=(n1+n2+n3)/3
-print("The average of n1,n2,n3 is ",average)
+n1 = float(input("Enter the first value: "))
+n2 = float(input("Enter the second value: "))
+n3 = float(input("Enter the third value: "))     
+avg = (n1+n2+n3)/3
+print(f"The average of {n1}, {n2}, {n3} is {avg}")
+
+
